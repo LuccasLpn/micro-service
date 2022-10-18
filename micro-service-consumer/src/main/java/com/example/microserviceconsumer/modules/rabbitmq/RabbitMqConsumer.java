03dca62af9ef9b9e75509af5365c1aee32908623
LuccasLpn <Luccas.lpn@outlook.com>
2022-10-17 21:29:38 -0300

    add postgres

---
+++ micro-service-consumer/src/main/java/com/example/microserviceconsumer/modules/rabbitmq/RabbitMqConsumer.java
@@ -0,0 +1,29 @@
+package com.example.microserviceconsumer.modules.rabbitmq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RabbitMqConsumer {
+
+    @RabbitListener(queues = "queue-order")
+    public String receiveOrder(String message) {
+        log.info("Message received: {}", message);
+        return message;
+    }
+
+    @RabbitListener(queues = "queue-payment")
+    public String receivePayment(String message) {
+        log.info("Message received: {}", message);
+        return message;
+    }
+
+    @RabbitListener(queues = "queue-shipping")
+    public String receiveShipping(String message) {
+        log.info("Message received: {}", message);
+        return message;
+    }
+
+}
--- micro-service-producer/src/main/java/br/com/microserviceproducer/MicroServiceProducerApplication.java
+++ micro-service-producer/src/main/java/br/com/microserviceproducer/MicroServiceProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroServiceApplication {
+public class MicroServiceProducerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroServiceApplication.class, args);
+        SpringApplication.run(MicroServiceProducerApplication.class, args);
     }
 
 }
