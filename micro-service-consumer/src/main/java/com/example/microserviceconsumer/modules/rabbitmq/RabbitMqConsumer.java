2683ae661e375687b291aa7e137faa769c7032df
LuccasLpn <Luccas.lpn@outlook.com>
2022-10-17 22:30:44 -0300

    add flyway init

--- micro-service-consumer/src/main/java/com/example/microserviceconsumer/modules/rabbitmq/RabbitMqConsumer.java
+++ micro-service-consumer/src/main/java/com/example/microserviceconsumer/modules/rabbitmq/RabbitMqConsumer.java
@@ -2,10 +2,8 @@ package com.example.microserviceconsumer.modules.rabbitmq;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
 public class RabbitMqConsumer {
 
     @RabbitListener(queues = "queue-order")
